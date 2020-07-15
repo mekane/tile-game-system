ed128be80d7e7ff529e17c17e3ccb3db2a9d8eec
Marty Kane <marty.kane@cmegroup.com>
2020-07-14 20:20:33 -0500

    Use Repository module with a simplistic in-memory data store

--- src/adapters/LocalGame/index.js
+++ src/adapters/LocalGame/index.js
@@ -1,14 +1,24 @@
 const UseCasesInit = require('../../useCases/index');
+const {Repository} = require('../../repository');
 
-const {inMemoryRepository} = require('../../../test/_mocks'); //TODO: make a different in-memory repo with tests
+function inMemoryDataStore(initialData) {
+    let repo = initialData || {};
+
+    return {
+        get: id => repo[id],
+        list: () => Object.values(repo),
+        put: obj => repo[obj.id] = obj,
+        debug: () => console.dir(repo)
+    }
+}
 
 /**
  * This wraps the use cases, injects in-memory repositories, and exports an
  * interface of JS functions (as a library) to control a tile game.
  */
 function init() {
-    const gameRepository = inMemoryRepository();
-    const scenarioRepository = inMemoryRepository();
+    const gameRepository = Repository(inMemoryDataStore());
+    const scenarioRepository = Repository(inMemoryDataStore());
     const useCases = UseCasesInit({gameRepository, scenarioRepository});
 
     return useCases;
