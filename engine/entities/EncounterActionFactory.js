13c6c67fc41b1e300de3d55435f915970022f76a
Marty Kane <marty.kane@gmail.com>
2021-05-18 16:44:28 -0500

    Add tests for real encounter factory and injecting actions

--- engine/entities/EncounterActionFactory.js
+++ engine/entities/EncounterActionFactory.js
@@ -3,14 +3,23 @@ const addUnit = require('./encounterActions/addUnit.js');
 const doneActivating = require('./encounterActions/markUnitDone.js');
 const moveUnit = require('./encounterActions/moveUnit.js');
 
-const actionByName = {
+const defaultActionByName = {
     activateunit: activateUnit,
     addunit: addUnit,
     markunitdone: doneActivating,
     moveunit: moveUnit
 }
 
-function EncounterActionFactory() {
+function EncounterActionFactory(additionalActionMap = {}) {
+
+    const actionByName = Object.assign({}, defaultActionByName)
+
+    Object.keys(additionalActionMap).forEach(actionName => {
+        const actionFunction = additionalActionMap[actionName];
+        if (typeof actionFunction === 'function')
+            actionByName[actionName.toLowerCase()] = actionFunction
+    })
+
     function getActionByName(actionName = '') {
         const name = typeof actionName === 'string' ? actionName.toLowerCase() : '';
 
--- test/engine/entities/EncounterActionFactory.spec.js
+++ test/engine/entities/EncounterActionFactory.spec.js
@@ -32,6 +32,46 @@ describe('The Encounter Action Factory', () => {
         expect(factory.getActionByName('moveUnit')).to.be.a('function')
     })
 
-    it(``, () => {
+    it(`can be injected with a map of additional action names and functions`, () => {
+        const factory = EncounterActionFactory({
+            test: () => 'test action function'
+        })
+
+        const testAction = factory.getActionByName('test')
+        expect(testAction()).to.equal('test action function')
+    })
+
+    it(`ignores non-functions in the injected action map`, () => {
+        let bogus0;
+        const factory = EncounterActionFactory({
+            bogus0,
+            bogus1: '',
+            bogus2: 12,
+            bogus3: [],
+            bogus4: {},
+        })
+
+        expect(factory.getActionByName('bogus0')).to.be.an('undefined')
+        expect(factory.getActionByName('bogus1')).to.be.an('undefined')
+        expect(factory.getActionByName('bogus2')).to.be.an('undefined')
+        expect(factory.getActionByName('bogus3')).to.be.an('undefined')
+        expect(factory.getActionByName('bogus4')).to.be.an('undefined')
+    })
+
+    it(`matches action names case-insensitive`, () => {
+        const factory = EncounterActionFactory({
+            test: () => 'test action function',
+            FOO: () => 'test action foo'
+        })
+
+        expect(factory.getActionByName('activateUnit')).to.be.a('function')
+        expect(factory.getActionByName('ACTIVATEUNIT')).to.be.a('function')
+        expect(factory.getActionByName('acTIvateUniT')).to.be.a('function')
+        expect(factory.getActionByName('test')).to.be.a('function')
+        expect(factory.getActionByName('TEST')).to.be.a('function')
+        expect(factory.getActionByName('TeSt')).to.be.a('function')
+        expect(factory.getActionByName('foo')).to.be.a('function')
+        expect(factory.getActionByName('FOO')).to.be.a('function')
+        expect(factory.getActionByName('FoO')).to.be.a('function')
     })
 });
